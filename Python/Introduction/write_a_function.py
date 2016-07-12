a223d414df1ef46d5c6b10e3451b6e370b198306
kevinmetheny <kevin.metheny@gmail.com>
2016-07-12 12:54:45 -0400

    Create write_a_function.py

---
+++ Python/Introduction/write_a_function.py
@@ -0,0 +1,12 @@
+Enter file contents here
+
+def is_leap(year):
+  leap=False
+  
+  if year%400==0:
+    return True
+  if year%100==0:
+    return Flase:
+  elif year%4==0:
+    return True
+  return False
